1d2a831e87eb2df567e7e24a7f6acb71d5b03f7a
Yadira Avila <pyavila@utpl.edu.ec>
2022-02-22 16:41:52 -0500

    version 2

--- Controladores/citasC.php
+++ Controladores/citasC.php
@@ -12,8 +12,7 @@ class CitasC{
 			$Did = substr($_GET["url"], 7);
 
 			$datosC = array("Did"=>$_POST["Did"], "Pid"=>$_POST["Pid"], "nyaC"=>$_POST["nyaC"], "Cid"=>$_POST["Cid"], "documentoC"=>$_POST["documentoC"], "fyhIC"=>$_POST["fyhIC"], "fyhFC"=>$_POST["fyhFC"]);
-			$datosC = array("Did"=>$_POST["Did"], "Cid"=>$_POST["Cid"], "nombreP"=>$_POST["nombreP"], "documentoP"=>$_POST["documentoP"], "fyhIC"=>$_POST["fyhIC"], "fyhFC"=>$_POST["fyhFC"]);
-
+		
 			$resultado = CitasM::EnviarCitaM($tablaBD, $datosC);
 
 			if($resultado == true){
